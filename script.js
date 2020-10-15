25c10b1b4b5d6b8fdd36806344fc0d33e58de137
Elan Zinkow <ezinkow@gmail.com>
2020-10-14 22:32:39 -0500

    commit updated code. printing out too many characters

--- script.js
+++ script.js
@@ -1,50 +1,67 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
-  // var password = generatePassword();
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+}
   var newPassword = "";
-  var passwordChars = "";
+  // var passwordChars = "";
 
   var upperCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowerCharacters = upperCharacters.toLowerCase();
   var specialCharacters = "!#$%&()*+,-'./:;<=>?@][^_`{|}~";
-  var numbers = "0123456789";
+  var numberCharacters = "0123456789";
 
-  var charactersNum = prompt("How many characters (between 8 and 128)?")
-  if((charactersNum < 8) || (charactersNum) > 128) {
-  alert("Password must be between 8 and 128 characters");
-  return;
+ function generatePassword() {
+ 
+  var charactersNum = prompt("How many characters (between 8 and 128)?");
+  while (charactersNum < 8 || charactersNum > 128) {
+    alert("Password must be between 8 and 128 characters");
+    charactersNum = prompt("How many characters (between 8 and 128)?");
   }
 
   var special = confirm("Do you want to use special characters?")
-  if(special === true) {
-    passwordChars += special;
-  }
-
   var numbers = confirm("Do you want to use numeric characters?")
-  if(numbers === true) {
-    passwordChars += numbers;
-  }
-
   var lower = confirm("Do you want to use lower case?")
-  if(lower === true) {
-    passwordChars += lower;
-  }
-
   var upper = confirm("do you want to use upper case?")
-  if(upper = true) {
-    passwordChars += upper;
-  }
+
+
+  var index ;
+
+  while (newPassword.length <= charactersNum) {
+    if(special === true) {
+      index = getRandomIndex(specialCharacters.length);
+      newPassword += specialCharacters[index];
+    }
+    if(numbers === true) {
+      index = getRandomIndex(numberCharacters.length);
+      newPassword += numberCharacters[index];
+    }
+    if(lower === true) {
+      index = getRandomIndex(lowerCharacters.length);
+      newPassword += lowerCharacters[index];
+    }
+    if(upper = true) {
+      index = getRandomIndex(upperCharacters.length);
+      newPassword += upperCharacters[index];
+      }
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+console.log(newPassword);
+
+return newPassword
+ }
+
+ function getRandomIndex(max) {
+   return Math.floor(Math.random() * max);
+ }
 
 
 
@@ -62,23 +79,37 @@ generateBtn.addEventListener("click", writePassword);
 
 // console.log(upperCharacters[for(var i = 0; i < )])
 
-// var randomUpper = (upperCharacters[Math.floor(Math.random()*upperCharacters.length)]);
-// var randomLower = (lowerCharacters[Math.floor(Math.random()*lowerCharacters.length)]);
-// var randomSpecial = (specialCharacters[Math.floor(Math.random()*specialCharacters.length)]);
-// var randomNumber = (numbers[Math.floor(Math.random()*numbers.length)]);
+
+// these all need to be a function, not a var
+// var randomUpper = upperCharacters[Math.floor(Math.random()*upperCharacters.length)];
+// var randomLower = lowerCharacters[Math.floor(Math.random()*lowerCharacters.length)];
+// var randomSpecial = specialCharacters[Math.floor(Math.random()*specialCharacters.length)];
+// var randomNumber = numbers[Math.floor(Math.random()*numbers.length)];
 
 // var password1 = [randomUpper,randomLower,randomSpecial,randomNumber];
 
-// for (var i = 0; i < charactersNum; i++) {
-//   console.log((password1[Math.floor(Math.random()*4)]))
+// for (var i = 0; i < 100; i++) {
+//   return((password1[Math.floor(Math.random()*4)]))
+// }
+
+// console.log(randomUpper)
+
+
+
+
+// // how many characters? must be 8 or more
+// // special characters?
+// // numeric characters?
+// // lower case?
+// // upper case?
+// // function that executes 72-73
+// function getRandomChar() {
+//   return password1[Math.floor(Math.random()*4)]
+
+
 // }
 
 
+// // var password = ""
 
-
-
-// how many characters? must be 8 or more
-// special characters?
-// numberic characters?
-// lower case?
-// upper case?
+// // and then +=
