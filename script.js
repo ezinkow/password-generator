dae8985b627f9d26c360274dbc3ae4ab685550c3
Elan Zinkow <ezinkow@gmail.com>
2020-10-17 15:01:26 -0500

    adding trashed code file and JS comments

--- script.js
+++ script.js
@@ -20,18 +20,19 @@ function writePassword() {
 
   passwordText.value = password;
 }
-
+// generate password function
  function generatePassword() {
 
   var charactersNum = prompt("How many characters (between 8 and 128)?");
 
   charactersNum = parseInt(charactersNum)
 
+// number of characters
   while (charactersNum < 8 || charactersNum > 128) {
     alert("Password must be between 8 and 128 characters");
     charactersNum = prompt("How many characters (between 8 and 128)?");
   }
-
+//  collection questions
   var special = confirm("Do you want to use special characters?")
   var numbers = confirm("Do you want to use numeric characters?")
   var lower = confirm("Do you want to use lower case characters?")
@@ -39,7 +40,7 @@ function writePassword() {
 
 
   var index ;
-
+// while loop with if statements collecting answers and pulling from "buckets"
   while (newPassword.length <= charactersNum) {
 
     if(special === true) {
@@ -59,63 +60,13 @@ function writePassword() {
       newPassword += upperCase[index];
       }
 }
-
-// console.log(newPassword);
+// console log new password
+console.log(newPassword);
 
 return newPassword
  }
 
-
+// get random function
  function getRandomIndex(max) {
   return Math.floor(Math.random() * max);
 }
-
-
-
-
-
-
-// for(var i = 0; i < charactersNum; i++) {
-//   console.log(Math.floor(Math.random() * 10))
-// }
-
-
-
-// console.log((upperCharacters[math.random(upperCharacters)])*upperCharacters.length);
-
-// console.log(upperCharacters[for(var i = 0; i < )])
-
-
-// these all need to be a function, not a var
-// var randomUpper = upperCharacters[Math.floor(Math.random()*upperCharacters.length)];
-// var randomLower = lowerCharacters[Math.floor(Math.random()*lowerCharacters.length)];
-// var randomSpecial = specialCharacters[Math.floor(Math.random()*specialCharacters.length)];
-// var randomNumber = numbers[Math.floor(Math.random()*numbers.length)];
-
-// var password1 = [randomUpper,randomLower,randomSpecial,randomNumber];
-
-// for (var i = 0; i < 100; i++) {
-//   return((password1[Math.floor(Math.random()*4)]))
-// }
-
-// console.log(randomUpper)
-
-
-
-
-// // how many characters? must be 8 or more
-// // special characters?
-// // numeric characters?
-// // lower case?
-// // upper case?
-// // function that executes 72-73
-// function getRandomChar() {
-//   return password1[Math.floor(Math.random()*4)]
-
-
-// }
-
-
-// // var password = ""
-
-// // and then +=
